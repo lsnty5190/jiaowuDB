805d138a1010a53dddb429080f5b62793ff5dfd8
lsnty5190 <1758213469@qq.com>
2021-12-15 22:59:51 +0800

    add intro

--- client/mock/user.js
+++ client/mock/user.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-14 16:17:15
+ * @LastEditTime: 2021-12-15 22:28:15
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \jiaowuDB\client\mock\user.js
+ */
 
 const tokens = {
   admin: {
--- client/src/views/dashboard/index.vue
+++ client/src/views/dashboard/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <p><div class="dashboard-text" align='center'>Welcome To JiaoWUDB!</div>
+    <p class='big'><div class="front-text" align="center">Contributers: 傅云濠 赵浩然 梁笑钰 李绍宁</div>
+    <img src="@/assets/front/ER.jpg" alt="error">
   </div>
 </template>
+
+
 
 <script>
 import { mapGetters } from 'vuex'
@@ -19,6 +23,18 @@ export default {
 
 <style lang="scss" scoped>
 .dashboard {
+  &-container {
+    margin: 50px;
+  }
+  &-text {
+    font-size: 60px;
+    line-height: 46px;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+.front {
   &-container {
     margin: 30px;
   }
